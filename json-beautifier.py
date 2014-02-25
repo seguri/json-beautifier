fffe66094c74b68503a72c88cb7fb4017b7bb2a5
Marco Seguri <marco.seguri@server.fake>
2014-02-25 18:40:36 +0100

    Refactor JSON management

--- json-beautifier.py
+++ json-beautifier.py
@@ -12,31 +12,23 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 )
 
 
-def is_json(s):
-    try:
-        json.loads(s)
-    except ValueError:
-        return False
-    return True
-
-
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.write('Make a POST with json=your_stringified_json to have it beautified.')
 
     def post(self):
-        json_ = self.request.get('json')
-        if json_ and is_json(json_):
+        try:
+            # Detect empty request and valid JSON
+            stripped_json = json.dumps(json.loads(self.request.get('json')))
+        except ValueError:
+            self.response.write('ERROR: You must provide a valid json.')
+        else:
             random_string = generate_random_string(length=16)
-            stripped_json = json.dumps(json.loads(json_))
             if memcache.add(random_string, stripped_json, MEMCACHE_EXPIRE):
                 self.response.write('%s/%s' % (self.request.host, random_string))
             else:
                 self.response.write('ERROR: Could not set cache.')
-        else:
-            self.response.write('ERROR: You must provide a valid json.')
-
 
 class CacheHandler(webapp2.RequestHandler):
 
